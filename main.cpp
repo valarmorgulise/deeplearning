d42af3d445e78ef60a673eab676bf9434a84d62f
xupeng <valarmorgulise@163.com>
2025-07-06 11:14:44 +0800

    nlp学习提交

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+  int x = 0;
+  if (x == 5) {
+    std::cout << "x is 5" << std::endl;
+  }
+  std::cout << "hello world" << std::endl;
+  return 0;
+
+  cout << "hello" << endl;
+}
